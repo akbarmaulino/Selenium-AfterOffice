ae66707bfc81fded015c1b9c89bf37d8408c4fe5
akbarmaulino <maulinoakbar8@gmail.com>
2025-08-06 20:41:37 +0700

    Day 14

--- src/test/java/seleniumPageFactory/cucumber/definitions/checkoutE2E.java
+++ src/test/java/seleniumPageFactory/cucumber/definitions/checkoutE2E.java
@@ -3,7 +3,7 @@ package seleniumPageFactory.cucumber.definitions;
 
 
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import seleniumPageFactory.pages.cartPages;
